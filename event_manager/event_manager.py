4804ee1998d46b8ae09820bed473dc81d3754982
Nils Söderman <rymdnisse@live.com>
2022-11-14 08:54:45 +0100

    Changed argument order of event / function

--- event_manager/event_manager.py
+++ event_manager/event_manager.py
@@ -4,63 +4,6 @@ from typing import Union, Hashable
 from types import FunctionType
 
 
-def bind(function: FunctionType, event: Hashable):
-    """
-    Bind a function to a event
-
-    ### Parameters:
-        - event: The event to bind to, identifed by a hasable object, e.g. a string.
-        - function: The function to bind
-    """
-    events = get_event_functions(event, True)
-    if function not in events:
-        events.append(function)
-
-
-def unbind(function: FunctionType, event: Hashable):
-    """
-    Unbind a function from a event
-
-    ### Parameters:
-        - function: The function to unbind
-        - event: The event to unbind to, identifed by a hasable object, e.g. a string.
-    """
-    events = get_event_functions(event, False)
-    if events and function in events:
-        events.remove(function)
-
-
-def unbind_all(event: Union[Hashable, None] = None):
-    """
-    Unbind all function either globally or from a spesific event
-
-    ### Parameters:
-        - event: The event to unbind all functions from, if event is None this function will unbind all functions from all events.
-    """
-    if event is None:
-        event_dict = get_event_dict(False)
-        if event_dict:
-            event_dict.clear()
-    else:
-        events = get_event_functions(event, False)
-        if events:
-            events.clear()
-
-
-def is_bound(function: FunctionType, event: Hashable):
-    """
-    Check if a function is bound to a event
-
-    ### Parameters:
-        - function: The function to unbind
-        - event: The event to unbind to, identifed by a hasable object, e.g. a string.
-    """
-    events = get_event_functions(event, False)
-    if events:
-        return function in events
-    return False
-
-
 def get_event_dict(ensure_exists: bool = False) -> Union[dict, None]:
     if ensure_exists and "event_manager_dict" not in globals():
         new_dict = {}
@@ -78,6 +21,63 @@ def get_event_functions(event: Hashable, ensure_exists: bool = False) -> Union[l
         return new_list
 
     return event_dict.get(event)
+
+
+def bind(event: Hashable, function: FunctionType):
+    """
+    Bind a function to a event
+
+    ### Parameters:
+        - event: The event to bind to, identifed by a hasable object, e.g. a string.
+        - function: The function to bind
+    """
+    events = get_event_functions(event, True)
+    if function not in events:
+        events.append(function)
+
+
+def unbind(event: Hashable, function: FunctionType):
+    """
+    Unbind a function from a event
+
+    ### Parameters:
+        - event: The event to unbind to, identifed by a hasable object, e.g. a string.
+        - function: The function to unbind
+    """
+    events = get_event_functions(event, False)
+    if events and function in events:
+        events.remove(function)
+
+
+def unbind_all(event: Union[Hashable, None] = None, function: Union[FunctionType, None] = None):
+    """
+    Unbind all function either globally or from a spesific event
+
+    ### Parameters:
+        - event: The event to unbind all functions from, if event is None this function will unbind all functions from all events.
+    """
+    if event is None:
+        event_dict = get_event_dict(False)
+        if event_dict:
+            event_dict.clear()
+    else:
+        events = get_event_functions(event, False)
+        if events:
+            events.clear()
+
+
+def is_bound(event: Hashable, function: FunctionType):
+    """
+    Check if a function is bound to a event
+
+    ### Parameters:
+        - event: The event to unbind to, identifed by a hasable object, e.g. a string.
+        - function: The function to unbind
+    """
+    events = get_event_functions(event, False)
+    if events:
+        return function in events
+    return False
 
 
 def emit(event: Hashable, *args, **kwargs):
