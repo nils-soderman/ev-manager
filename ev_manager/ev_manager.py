a271317bc29e992f3d2393523286012d5aac3448
Nils Söderman <rymdnisse@live.com>
2023-09-24 17:50:16 +0200

    Added `by_ref` optional argument

--- ev_manager/ev_manager.py
+++ ev_manager/ev_manager.py
@@ -56,26 +56,34 @@ def bind(event: Hashable, function: Callable):
         functions.append(function)
 
 
-def unbind(event: Hashable, function: Callable):
+def unbind(event: Hashable, function: Callable, by_ref: bool = False):
     """
     Unbind a function from a event
 
     ### Parameters:
         - event: The event to unbind to, identifed by a hasable object, e.g. a string.
         - function: The function to unbind
+        - by_ref: If True, unbind the function by reference, otherwise unbind by name
     """
     functions = get_event_functions(event, False)
-    if functions and function in functions:
-        functions.remove(function)
+    if functions:
+        if by_ref:
+            if function in functions:
+                functions.remove(function)
+        else:
+            for func in functions:
+                if func.__module__ == function.__module__ and func.__name__ == function.__name__:
+                    functions.remove(func)
 
 
-def unbind_all(*, event: Hashable | None = None, function: Callable | None = None):
+def unbind_all(*, event: Hashable | None = None, function: Callable | None = None, by_ref: bool = False):
     """
     Unbind all functions from a event or unbind a function from all events.
 
     ### Parameters:
         - event: If provided, unbind all functions from this event
         - function: If provided, unbind this function from all events
+        - by_ref: If True, unbind the function by reference, otherwise unbind by name (Only applicable if function is provided)
     """
     if event:
         functions = get_event_functions(event, False)
@@ -85,21 +93,32 @@ def unbind_all(*, event: Hashable | None = None, function: Callable | None = Non
         event_dict = get_event_dict(False)
         if event_dict:
             for functions in event_dict.values():
-                if function in functions:
-                    functions.remove(function)
+                if by_ref:
+                    if function in functions:
+                        functions.remove(function)
+                else:
+                    for func in list(functions):
+                        if func.__module__ == function.__module__ and func.__name__ == function.__name__:
+                            functions.remove(func)
 
 
-def is_bound(event: Hashable, function: Callable) -> bool:
+def is_bound(event: Hashable, function: Callable, by_ref: bool = False) -> bool:
     """
     Check if a function is bound to a event
 
     ### Parameters:
         - event: The event to unbind to, identifed by a hasable object, e.g. a string.
         - function: The function to unbind
+        - by_ref: If True, check if the function is bound by reference, otherwise check by name
     """
     functions = get_event_functions(event, False)
     if functions:
-        return function in functions
+        if by_ref:
+            return function in functions
+        else:
+            for func in functions:
+                if func.__module__ == function.__module__ and func.__name__ == function.__name__:
+                    return True
     return False
 
 
